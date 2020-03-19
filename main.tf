3d987c5469a5101cea349723659aeaa9a8a052c8
caleb15 <caleb@15five.com>
2020-03-19 10:45:10 -0700

    count not needed for source_ids

--- main.tf
+++ main.tf
@@ -8,12 +8,11 @@
 data "aws_caller_identity" "default" {}
 
 resource "aws_db_event_subscription" "default" {
-  count       = length(var.db_instance_ids)
   name_prefix = "rds-event-sub"
   sns_topic   = var.aws_sns_topic_arn
 
   source_type = "db-instance"
-  source_ids  = [var.db_instance_ids[count.index]]
+  source_ids  = var.db_instance_ids
 
   event_categories = [
     "failure",
