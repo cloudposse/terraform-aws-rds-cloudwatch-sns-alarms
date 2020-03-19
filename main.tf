1d1246a09dc7c2919962df6a170813cae37f1867
caleb15 <caleb@15five.com>
2020-03-19 14:54:04 -0700

    sns topic policy not needed

--- main.tf
+++ main.tf
@@ -1,10 +1,3 @@
-# resource "aws_cloudwatch_event_target" "sns" {
-#   rule       = "${aws_cloudwatch_event_rule.default.name}"
-#   target_id  = "SendToSNS"
-#   arn        = "${var.sns_topic_arn}"
-#   depends_on = ["aws_cloudwatch_event_rule.default"]
-#   input      = "${var.sns_message_override}"
-# }
 data "aws_caller_identity" "default" {}
 
 resource "aws_db_event_subscription" "default" {
@@ -17,70 +10,4 @@ resource "aws_db_event_subscription" "default" {
   event_categories = [
     "failure",
   ]
-
-  depends_on = [aws_sns_topic_policy.default]
-}
-
-resource "aws_sns_topic_policy" "default" {
-  arn    = var.aws_sns_topic_arn
-  policy = data.aws_iam_policy_document.sns_topic_policy.json
-}
-
-data "aws_iam_policy_document" "sns_topic_policy" {
-  policy_id = "__default_policy_ID"
-
-  statement {
-    sid = "__default_statement_ID"
-
-    actions = [
-      "SNS:Subscribe",
-      "SNS:SetTopicAttributes",
-      "SNS:RemovePermission",
-      "SNS:Receive",
-      "SNS:Publish",
-      "SNS:ListSubscriptionsByTopic",
-      "SNS:GetTopicAttributes",
-      "SNS:DeleteTopic",
-      "SNS:AddPermission",
-    ]
-
-    effect    = "Allow"
-    resources = [var.aws_sns_topic_arn]
-
-    principals {
-      type        = "AWS"
-      identifiers = ["*"]
-    }
-
-    condition {
-      test     = "StringEquals"
-      variable = "AWS:SourceOwner"
-
-      values = [
-        data.aws_caller_identity.default.account_id,
-      ]
-    }
-  }
-
-  statement {
-    sid       = "Allow CloudwatchEvents"
-    actions   = ["sns:Publish"]
-    resources = [var.aws_sns_topic_arn]
-
-    principals {
-      type        = "Service"
-      identifiers = ["events.amazonaws.com"]
-    }
-  }
-
-  statement {
-    sid       = "Allow RDS Event Notification"
-    actions   = ["sns:Publish"]
-    resources = [var.aws_sns_topic_arn]
-
-    principals {
-      type        = "Service"
-      identifiers = ["rds.amazonaws.com"]
-    }
-  }
 }
