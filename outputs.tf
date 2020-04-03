241697f0b8bc5b4366a02dfa8faffc494c5d6ba3
Mat Geist <matgeist@gmail.com>
2020-04-03 13:59:41 -0700

    Add SNS Topic name

--- outputs.tf
+++ outputs.tf
@@ -3,3 +3,7 @@ output "sns_topic_arn" {
   value       = aws_sns_topic.default.arn
 }
 
+output "sns_topic_name" {
+  description = "The Name of the SNS topic"
+  value       = aws_sns_topic.default.name
+}
