4011b3199c36126ef74658f8fbdada4e44ffa87f
westonplatter <westonplatter@gmail.com>
2021-02-05 13:39:28 -0700

    finish modifying sns topic creation in alarms.

--- alarms.tf
+++ alarms.tf
@@ -25,8 +25,8 @@ resource "aws_cloudwatch_metric_alarm" "burst_balance_too_low" {
   statistic           = "Average"
   threshold           = local.thresholds["BurstBalanceThreshold"]
   alarm_description   = "Average database storage burst balance over last 10 minutes too low, expect a significant performance drop soon"
-  alarm_actions       = [aws_sns_topic.default.arn]
-  ok_actions          = [aws_sns_topic.default.arn]
+  alarm_actions       = [sns_topic_arn]
+  ok_actions          = [sns_topic_arn]
 
   dimensions = {
     DBInstanceIdentifier = var.db_instance_id
@@ -44,8 +44,8 @@ resource "aws_cloudwatch_metric_alarm" "cpu_utilization_too_high" {
   statistic           = "Average"
   threshold           = local.thresholds["CPUUtilizationThreshold"]
   alarm_description   = "Average database CPU utilization over last 10 minutes too high"
-  alarm_actions       = [aws_sns_topic.default.arn]
-  ok_actions          = [aws_sns_topic.default.arn]
+  alarm_actions       = [sns_topic_arn]
+  ok_actions          = [sns_topic_arn]
 
   dimensions = {
     DBInstanceIdentifier = var.db_instance_id
@@ -63,8 +63,8 @@ resource "aws_cloudwatch_metric_alarm" "cpu_credit_balance_too_low" {
   statistic           = "Average"
   threshold           = local.thresholds["CPUCreditBalanceThreshold"]
   alarm_description   = "Average database CPU credit balance over last 10 minutes too low, expect a significant performance drop soon"
-  alarm_actions       = [aws_sns_topic.default.arn]
-  ok_actions          = [aws_sns_topic.default.arn]
+  alarm_actions       = [sns_topic_arn]
+  ok_actions          = [sns_topic_arn]
 
   dimensions = {
     DBInstanceIdentifier = var.db_instance_id
@@ -82,8 +82,8 @@ resource "aws_cloudwatch_metric_alarm" "disk_queue_depth_too_high" {
   statistic           = "Average"
   threshold           = local.thresholds["DiskQueueDepthThreshold"]
   alarm_description   = "Average database disk queue depth over last 10 minutes too high, performance may suffer"
-  alarm_actions       = [aws_sns_topic.default.arn]
-  ok_actions          = [aws_sns_topic.default.arn]
+  alarm_actions       = [sns_topic_arn]
+  ok_actions          = [sns_topic_arn]
 
   dimensions = {
     DBInstanceIdentifier = var.db_instance_id
@@ -101,8 +101,8 @@ resource "aws_cloudwatch_metric_alarm" "freeable_memory_too_low" {
   statistic           = "Average"
   threshold           = local.thresholds["FreeableMemoryThreshold"]
   alarm_description   = "Average database freeable memory over last 10 minutes too low, performance may suffer"
-  alarm_actions       = [aws_sns_topic.default.arn]
-  ok_actions          = [aws_sns_topic.default.arn]
+  alarm_actions       = [sns_topic_arn]
+  ok_actions          = [sns_topic_arn]
 
   dimensions = {
     DBInstanceIdentifier = var.db_instance_id
@@ -120,8 +120,8 @@ resource "aws_cloudwatch_metric_alarm" "free_storage_space_too_low" {
   statistic           = "Average"
   threshold           = local.thresholds["FreeStorageSpaceThreshold"]
   alarm_description   = "Average database free storage space over last 10 minutes too low"
-  alarm_actions       = [aws_sns_topic.default.arn]
-  ok_actions          = [aws_sns_topic.default.arn]
+  alarm_actions       = [sns_topic_arn]
+  ok_actions          = [sns_topic_arn]
 
   dimensions = {
     DBInstanceIdentifier = var.db_instance_id
@@ -139,8 +139,8 @@ resource "aws_cloudwatch_metric_alarm" "swap_usage_too_high" {
   statistic           = "Average"
   threshold           = local.thresholds["SwapUsageThreshold"]
   alarm_description   = "Average database swap usage over last 10 minutes too high, performance may suffer"
-  alarm_actions       = [aws_sns_topic.default.arn]
-  ok_actions          = [aws_sns_topic.default.arn]
+  alarm_actions       = [sns_topic_arn]
+  ok_actions          = [sns_topic_arn]
 
   dimensions = {
     DBInstanceIdentifier = var.db_instance_id
