a7d162aa0fd4e8e65e8b8b316b01a698b612a321
Joe Niland <joe@originalmind.com.au>
2022-07-03 09:12:38 +1000

    avoid deprecation warning for aws_db_instance.name

--- examples/complete/main.tf
+++ examples/complete/main.tf
@@ -9,7 +9,7 @@ resource "aws_db_instance" "default" {
   engine_version       = "5.7"
   instance_class       = "db.t2.micro"
   identifier           = module.this.id
-  name                 = "mydb"
+  db_name              = "mydb"
   username             = "foo"
   password             = "foobarbaz"
   parameter_group_name = "default.mysql5.7"
